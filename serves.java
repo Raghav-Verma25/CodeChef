67295b380e49966570d65d35c2c5128ce29e8812
Raghav Verma <91414579+Raghav-Verma25@users.noreply.github.com>
2022-02-02 20:08:35 +0530

    Create serves.java

---
+++ serves.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int tesst = sc.nextInt();
+		while(tesst!=0){ 
+		    
+		    int a = sc.nextInt();
+		    int ans = (a/2)+1;
+		    System.out.println(ans);
+		    
+		    
+		    tesst = tesst-1;
+		}
+	}
+}
